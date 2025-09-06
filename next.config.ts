3666caa2e6cb9b5354d91fc375e3bb1fda5dd8e2
塚本頼範 <tsukamotoyorinori@I-roy-Mac-mini.local>
2025-09-06 09:43:46 +0900

    Fix next.config.ts and update dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    allowedDevOrigins: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "http://192.168.0.18:3000",
-      "http://192.168.0.18:3001",
-    ],
+   
   },
 };
 
